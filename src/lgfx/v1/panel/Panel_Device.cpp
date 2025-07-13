5e3e320c235128cfb9327a720caa46d38146cc91
neil-brik <neil.benn@brikwiz.com>
2025-07-13 12:41:06 +0100

    is bus set?

--- src/lgfx/v1/panel/Panel_Device.cpp
+++ src/lgfx/v1/panel/Panel_Device.cpp
@@ -35,8 +35,10 @@ namespace lgfx
 
   void Panel_Device::setBus(IBus* bus)
   {
-    static Bus_NULL nullobj;
+    static Bus_NULL nullobj;#
+    Log.noticeln("Panel_Device::setBus %d", bus);
     _bus = bus ? bus : &nullobj;
+    Log.noticeln("Panel_Device::setBus complete %d", _bus);
   }
 
   void Panel_Device::setBrightness(uint8_t brightness)
